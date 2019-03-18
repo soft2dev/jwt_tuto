bb93e95dfbd0188cb1783427a47b7acb06a92360
sen_web <sen_web@xins-iMac.local>
2019-03-18 16:35:06 +0800

    JWT tutorial

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+
+const app = express();
+
+app.get('/api', (req,res) => {
+    res.json({
+        message:'Welcom to the API'
+    });
+});
+
+app.post('/api/posts', verifyToken, (req,res) => {
+    jwt.verify(req.token, 'secretkey', { expiresIn: '30s' }, (err, authData) => {
+        if(err) {
+            res.sendStatus(403);
+        } else {
+            res.json({
+                message: 'Post created...',
+                authData
+            })
+        }
+    })
+});
+
+app.post('/api/login', (req,res) => {
+    //Mock user
+    const user = {
+        id: 1,
+        username: 'admin',
+        email: 'sen.web@hotmail.com'
+    }
+    jwt.sign({user}, 'secretkey', (err, token) => {
+        res.json({
+            token
+        });
+    });
+});
+
+// FORMAT OF TOKEN
+// Authorization: Bearer <access_token>
+
+// Verify Token
+function verifyToken(req, res, next) {
+    // Get auth header value
+    const bearerHeader = req.headers['authorization'];
+    // Check if bearer is undefined
+    if(typeof bearerHeader !== 'undefined') {
+        // Spilit at the space
+        const bearer = bearerHeader.split(' ');
+        //Get token from array
+        const bearerToken = bearer[1];
+        // Set the token
+        req.token = bearerToken;
+        // Next middleware
+        next();
+    } else {
+        // Forbidden
+        res.sendStatus(403);
+    }
+}
+
+app.listen(5000,()=>console.log('Server started on port 5000'));
